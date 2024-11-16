2100a6e6f34ba5b8c5ace502e5075e44dc0850a8
Mahmoud Harmouch <oss@wiseai.dev>
2024-11-16 02:35:47 +0200

    fix: update axum config && deploy

--- src/pages/aibook.rs
+++ src/pages/aibook.rs
@@ -6,7 +6,7 @@ pub fn AIBook() -> Element {
         iframe {
             class: "w-full min-h-full",
             // TODO: Deploy app
-            src: "https://booksai.netlify.app",
+            src: "https://aibook-8syx.onrender.com/",
             scrolling: "auto",
             style: "border: none; width: 100vw; height: 100vh; overflow: hidden; position: fixed; top: 0; left: 0;",
             frame_border: "0",
