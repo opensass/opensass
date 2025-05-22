bcc60de681988a60f41070d38abffbf4a38f7212
wiseaidev <oss@wiseai.dev>
2025-05-22 19:27:22 +0300

    fix: mv `http` `https`

--- src/pages/kit.rs
+++ src/pages/kit.rs
@@ -5,7 +5,7 @@ pub fn Kit() -> Element {
     rsx! {
         iframe {
             class: "w-full min-h-full",
-            src: "http://opensass.netlify.app/",
+            src: "https://opensass.netlify.app/",
             scrolling: "auto",
             style: "border: none; width: 100vw; height: 100vh; overflow: hidden; position: fixed; top: 0; left: 0;",
             frame_border: "0",
